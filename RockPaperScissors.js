7adf0beef9a15a189c3c6d40da40066e39f741ec
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2026-06-30 18:06:39 +0500

    Implement the function to play game round

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -2,9 +2,10 @@ function getComputerChoice(){
     let choiceNumber = Math.floor(Math.random()*3)+1;
     return getChoiceThroughNumber(choiceNumber);
     
+    
 }
 function getHumanChoice(){
-    let userChoice = 0;
+    let userChoiceNumber = 0;
     do {
         userChoiceNumber = +prompt("Choose your weapon:\n1.Rock\n2.Paper\n3.Scissors\nEnter your choice number:");
     } while (!(userChoiceNumber>0&&userChoiceNumber<4));
@@ -24,13 +25,40 @@ function getChoiceThroughNumber(choiceNumber=0){
     }
 }
 let humanScore =0;
-let globalScore =0;
+let computerScore =0;
 const userChoice =getHumanChoice();
 const computerChoice = getComputerChoice();
-printChoices(userChoice,computerChoice);
+playRound(userChoice,computerChoice);
 
 
+function playRound(humanSelection="", computerSelection=""){
+    printChoices(humanSelection,computerSelection);
+     //.toLowerCase() is not necessary but if i let user give string input , i will not have to change the play round again
+    if (humanSelection.toLowerCase()===computerSelection.toLowerCase()){
+      console.log("Its a draw!");
+      humanScore++;
+      computerScore++;
+    }
+   
+    else if (humanSelection.toLowerCase()==="paper"&&computerSelection.toLowerCase()==="rock"){
+        console.log("You won!");
+        humanScore++;
+    }
+    else if (humanSelection.toLowerCase()==="scissors"&&computerSelection.toLowerCase()==="paper"){
+        console.log("You won!");
+        humanScore++;
+    }
+    else if (humanSelection.toLowerCase()==="rock"&&computerSelection.toLowerCase()==="scissors"){
+        console.log("You won!");
+        humanScore++;
+    }
+    else{
+        console.log("You lost!");
+        computerScore++;
+    }
+    
 
-function printChoices(humanSelection, computerSelection){
-    console.log(`You chose:${humanSelection}\nComputer chose:${computerSelection}`);
+}
+function printChoices(humanSelection="", computerSelection=""){
+    console.log(`You chose:${humanSelection}\nComputer chose: ${computerSelection}`);
 }
