6845e65fb5c5a7e73572ff45374fb826c8cb411c
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2026-06-30 17:11:00 +0500

    Move the choice based on number logic to a seperate function

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,14 +1,20 @@
 function getComputerChoice(){
-    let choiceNumber = Math.floor(Math.random()*3);
+    let choiceNumber = Math.floor(Math.random()*3)+1;
+    return getChoiceThroughNumber(choiceNumber);
+    
+}
+
+function getChoiceThroughNumber(choiceNumber=0){
     switch(choiceNumber){
-        case 0:
-        return "Rock";
         case 1:
-        return "Paper";
+        return "Rock";
         case 2:
+        return "Paper";
+        case 3:
         return "Scissors";
         default:
         return "Not a valid choice number"
     }
 }
+
 
