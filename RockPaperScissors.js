26dd30d41d5d55d66286f4d520206dcc20f76ed0
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2026-06-30 18:27:50 +0500

    Implement the play Game function
    Refactor the play round function to move the score manipulation
    to play game and make it return a string based on winner.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice(){
     let choiceNumber = Math.floor(Math.random()*3)+1;
     return getChoiceThroughNumber(choiceNumber);
@@ -24,11 +25,30 @@ function getChoiceThroughNumber(choiceNumber=0){
         return "Not a valid choice number"
     }
 }
-let humanScore =0;
-let computerScore =0;
-const userChoice =getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(userChoice,computerChoice);
+function playGame(){
+    let humanScore =0;
+    let computerScore =0;
+    const numberOfRounds =5;    
+    for (let i =0;i<numberOfRounds;i++){
+        const userChoice =getHumanChoice();
+        const computerChoice = getComputerChoice();
+        let result =playRound(userChoice,computerChoice,humanScore,computerScore);
+        switch(result){
+            case "win":
+            humanScore++;
+            break;
+            case "lose":
+            computerScore++;
+            break;
+            default:
+            humanScore++;
+            computerScore;;
+        }
+        printRoundScores(humanScore,computerScore,i);
+        
+    }
+}
+
 
 
 function playRound(humanSelection="", computerSelection=""){
@@ -36,25 +56,26 @@ function playRound(humanSelection="", computerSelection=""){
      //.toLowerCase() is not necessary but if i let user give string input , i will not have to change the play round again
     if (humanSelection.toLowerCase()===computerSelection.toLowerCase()){
       console.log("Its a draw!");
-      humanScore++;
-      computerScore++;
+     
     }
    
     else if (humanSelection.toLowerCase()==="paper"&&computerSelection.toLowerCase()==="rock"){
         console.log("You won!");
-        humanScore++;
+        return "win";
+        
     }
     else if (humanSelection.toLowerCase()==="scissors"&&computerSelection.toLowerCase()==="paper"){
         console.log("You won!");
-        humanScore++;
+        return "win";
+;
     }
     else if (humanSelection.toLowerCase()==="rock"&&computerSelection.toLowerCase()==="scissors"){
-        console.log("You won!");
-        humanScore++;
+        return window;
+        
     }
     else{
-        console.log("You lost!");
-        computerScore++;
+        return "lose";
+        
     }
     
 
@@ -62,7 +83,7 @@ function playRound(humanSelection="", computerSelection=""){
 function printChoices(humanSelection="", computerSelection=""){
     console.log(`You chose:${humanSelection}\nComputer chose: ${computerSelection}`);
 }
-function printRoundScores(roundNumber=1,humanScore,computerScore){
+function printRoundScores(humanScore=0,computerScore=0,roundNumber=1){
     console.log(`Round${roundNumber} Scores:`);
     console.log(`Your Score:${humanScore}\nComputer Score:${computerScore}`);
 }
